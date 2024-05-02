59ae3c044da957ba2b0479121377177f946a7e15
Gabriel Henrique Bueno <gabrielhenriquesi1996@gmail.com>
2024-05-02 15:16:10 -0300

    added gets e posts para teste

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,93 @@
 import { PrismaClient } from "@prisma/client";
+import express from "express";
 
 const prisma = new PrismaClient();
+const app = express();
 
-async function main() {
-  const newReview = await prisma.review.create({
-    data: {
-      rating: 4.5,
-      approximateDate: "02/05/2024",
-      text: "Excelente serviço",
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      published: false,
-    },
+app.use(express.json());
+
+app.get("/business", async (req, res) => {
+  const business = await prisma.business.findMany();
+  res.json(business);
+});
+
+app.get("/reviews", async (req, res) => {
+  const reviews = await prisma.review.findMany({});
+  res.json(reviews);
+});
+
+app.get(`/reviews/:id`, async (req, res) => {
+  const { id } = req.params;
+  const reviews = await prisma.review.findUnique({
+    where: { id: String(id) },
   });
-  console.log("Created new review: ", newReview);
+  res.json(reviews);
+});
+
+////////////////////////////////////////////////////////////////////////
+
+app.post(`/createBusiness`, async (req, res) => {
+  const { name, linkMap, linkReview, createdAt, updatedAt } = req.body;
 
   const newBusiness = await prisma.business.create({
     data: {
-      name: "Pérola negra Moda Intima",
-      linkMap: "https://github.com/",
-      linkReview: "https://google.com/",
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      reviews: {
-        connect: {
-          id: newReview.id,
-        },
-      },
+      name,
+      linkMap,
+      linkReview,
+      createdAt,
+      updatedAt,
     },
   });
-  console.log("Created new business: ", newBusiness);
+  res.json(newBusiness);
+});
 
-  const allBusiness = await prisma.business.findMany({
-    include: { reviews: true },
+app.post(`/createReview`, async (req, res) => {
+  const { rating, approximateDate, text, createdAt, updatedAt } = req.body;
+  const newReview = await prisma.review.create({
+    data: {
+      rating,
+      approximateDate,
+      text,
+      createdAt,
+      updatedAt,
+    },
   });
-  console.log("All business: ");
-  console.dir(allBusiness, { depth: null });
-}
+  res.json(newReview);
+});
 
-main()
-  .catch((e) => console.error(e))
-  .finally(async () => await prisma.$disconnect());
+/////////////////////////////////////////////////////////////////////
+
+app.put(`/updateBusiness/:id`, async (req, res) => {
+  const { id } = req.params;
+  const { name, linkMap, linkReview, createdAt, updatedAt } = req.body;
+
+  try {
+    const existingBusiness = await prisma.business.findUnique({
+      where: { id },
+    });
+
+    if (!existingBusiness) {
+      return res.status(404).json({ error: "Empresa não Existe" });
+    }
+
+    const updatedBusiness = await prisma.business.update({
+      where: { id },
+      data: {
+        name,
+        linkMap,
+        linkReview,
+        createdAt,
+        updatedAt,
+      },
+    });
+
+    res.json(updatedBusiness);
+  } catch (error) {
+    console.error("Error updating business:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.listen(3000, () =>
+  console.log("REST API server ready at: http://localhost:3000")
+);
