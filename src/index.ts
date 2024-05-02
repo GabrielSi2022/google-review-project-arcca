68f934091b937c398fa7432d3d1c188bef23fcbb
Gabriel Henrique Bueno <gabrielhenriquesi1996@gmail.com>
2024-05-02 19:06:42 -0300

    function main init api

--- src/index.ts
+++ src/index.ts
@@ -1,93 +1,74 @@
-import { PrismaClient } from "@prisma/client";
-import express from "express";
+// import { PrismaClient } from "@prisma/client";
+// import express from "express";
 
-const prisma = new PrismaClient();
-const app = express();
+import { ApiExpress } from "./api/express/api.express";
+import { BusinessController } from "./api/express/controllers/business/business.controller";
 
-app.use(express.json());
+// const prisma = new PrismaClient();
+// const app = express();
 
-app.get("/business", async (req, res) => {
-  const business = await prisma.business.findMany();
-  res.json(business);
-});
+// app.use(express.json());
 
-app.get("/reviews", async (req, res) => {
-  const reviews = await prisma.review.findMany({});
-  res.json(reviews);
-});
+// app.get("/business", async (req, res) => {
+//   const business = await prisma.business.findMany();
+//   res.json(business);
+// });
 
-app.get(`/reviews/:id`, async (req, res) => {
-  const { id } = req.params;
-  const reviews = await prisma.review.findUnique({
-    where: { id: String(id) },
-  });
-  res.json(reviews);
-});
+// app.get("/reviews", async (req, res) => {
+//   const reviews = await prisma.review.findMany({});
+//   res.json(reviews);
+// });
 
-////////////////////////////////////////////////////////////////////////
+// app.get(`/reviews/:id`, async (req, res) => {
+//   const { id } = req.params;
+//   const reviews = await prisma.review.findUnique({
+//     where: { id: String(id) },
+//   });
+//   res.json(reviews);
+// });
 
-app.post(`/createBusiness`, async (req, res) => {
-  const { name, linkMap, linkReview, createdAt, updatedAt } = req.body;
+// ////////////////////////////////////////////////////////////////////////
 
-  const newBusiness = await prisma.business.create({
-    data: {
-      name,
-      linkMap,
-      linkReview,
-      createdAt,
-      updatedAt,
-    },
-  });
-  res.json(newBusiness);
-});
+// app.post(`/createBusiness`, async (req, res) => {
+//   const { name, linkMap, linkReview, createdAt, updatedAt } = req.body;
 
-app.post(`/createReview`, async (req, res) => {
-  const { rating, approximateDate, text, createdAt, updatedAt } = req.body;
-  const newReview = await prisma.review.create({
-    data: {
-      rating,
-      approximateDate,
-      text,
-      createdAt,
-      updatedAt,
-    },
-  });
-  res.json(newReview);
-});
+//   const newBusiness = await prisma.business.create({
+//     data: {
+//       name,
+//       linkMap,
+//       linkReview,
+//       createdAt,
+//       updatedAt,
+//     },
+//   });
+//   res.json(newBusiness);
+// });
 
-/////////////////////////////////////////////////////////////////////
+// app.post(`/createReview`, async (req, res) => {
+//   const { rating, approximateDate, text, createdAt, updatedAt } = req.body;
+//   const newReview = await prisma.review.create({
+//     data: {
+//       rating,
+//       approximateDate,
+//       text,
+//       createdAt,
+//       updatedAt,
+//     },
+//   });
+//   res.json(newReview);
+// });
 
-app.put(`/updateBusiness/:id`, async (req, res) => {
-  const { id } = req.params;
-  const { name, linkMap, linkReview, createdAt, updatedAt } = req.body;
+// app.listen(3000, () =>
+//   console.log("REST API server ready at: http://localhost:3000")
+// );
 
-  try {
-    const existingBusiness = await prisma.business.findUnique({
-      where: { id },
-    });
+function main() {
+  const api = ApiExpress.build();
 
-    if (!existingBusiness) {
-      return res.status(404).json({ error: "Empresa não Existe" });
-    }
+  const controller = BusinessController.build();
+  api.addGetRoute("/business", controller.list);
+  api.addPostRoute("/createBusiness", controller.create);
+  api.start(3000);
+}
 
-    const updatedBusiness = await prisma.business.update({
-      where: { id },
-      data: {
-        name,
-        linkMap,
-        linkReview,
-        createdAt,
-        updatedAt,
-      },
-    });
-
-    res.json(updatedBusiness);
-  } catch (error) {
-    console.error("Error updating business:", error);
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
-
-app.listen(3000, () =>
-  console.log("REST API server ready at: http://localhost:3000")
-);
+main();
