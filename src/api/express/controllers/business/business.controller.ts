31c272a46b8fcb84e167232650de1dcefbc9936a
Gabriel Henrique Bueno <gabrielhenriquesi1996@gmail.com>
2024-05-02 19:07:55 -0300

    added businnes controller

---
+++ src/api/express/controllers/business/business.controller.ts
@@ -0,0 +1,50 @@
+import { BusinessRepositoryPrisma } from "../../../../repositories/business/prisma/business.repository.prisma";
+import { BusinessServiceImplementation } from "../../../../services/business/implementation/business.service.implementation";
+import { prisma } from "../../../../util/prisma.util";
+import { Request, Response } from "express";
+
+export class BusinessController {
+  private constructor() {}
+
+  public static build() {
+    return new BusinessController();
+  }
+
+  public async create(req: Request, res: Response) {
+    const { name, linkMap, linkReview, createdAt, updatedAt } = req.body;
+
+    const aRepository = BusinessRepositoryPrisma.build(prisma);
+    const aService = BusinessServiceImplementation.build(aRepository);
+
+    const output = await aService.create(
+      name,
+      linkMap,
+      linkReview,
+      createdAt,
+      updatedAt
+    );
+
+    const data = {
+      name: output.name,
+      linkMap: output.linkMap,
+      linkReview: output.linkReview,
+      createdAt: output.createdAt,
+      updatedAt: output.updatedAt,
+    };
+
+    res.status(201).json(data).send();
+  }
+
+  public async list(req: Request, res: Response) {
+    const aRepository = BusinessRepositoryPrisma.build(prisma);
+    const aService = BusinessServiceImplementation.build(aRepository);
+
+    const output = await aService.list();
+
+    const data = {
+      business: output.business,
+    };
+
+    res.status(200).json(data).send();
+  }
+}
