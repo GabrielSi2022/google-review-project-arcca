87d26bb85f2a621301d94462222e58fb018a0559
Gabriel Henrique Bueno <gabrielhenriquesi1996@gmail.com>
2024-05-06 14:43:54 -0300

    att nomeclatura

--- src/components/getReviews/index.ts
+++ src/components/getReviews/index.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 import { Reviews } from "../../entities/reviews";
 
-async function fetchReviews(storeNumber: number): Promise<any[]> {
-  const query = findQueryByStoreNumber(storeNumber);
+async function fetchReviews(storeIdentifier: number): Promise<any[]> {
+  const query = findQueryByStoreIdentifier(storeIdentifier);
   let paginationToken: any = null;
   let responsesList: any[] = [];
   let response: any;
@@ -11,7 +11,7 @@ async function fetchReviews(storeNumber: number): Promise<any[]> {
 
   let maxPages = 3;
   do {
-    const paginatedQuery = insertStringIntoString(query, paginationToken);
+    const paginatedQuery = insertTokenIntoQuery(query, paginationToken);
     try {
       response = await axios.get(paginatedQuery);
       structuredResponse = parseResponse(response.data);
@@ -46,7 +46,7 @@ async function fetchReviews(storeNumber: number): Promise<any[]> {
   return reviews;
 }
 
-function insertStringIntoString(
+function insertTokenIntoQuery(
   originalString: string,
   insertedString: string | null
 ): string {
@@ -67,13 +67,13 @@ function parseResponse(jsonString: string): any[] {
   return JSON.parse(cleanedJson);
 }
 
-function findQueryByStoreNumber(storeNumber: number): string {
+function findQueryByStoreIdentifier(storeIdentifier: number): string {
   const storeUrls: Record<number, string> = {
     0: "https://www.google.com.br/maps/rpc/listugcposts?authuser=0&hl=pt-BR&gl=br&pb=!1m8!1s0x94b64399cc827d1d%3A0xcfd43c9cb0e9088c!3s!6m4!4m1!1e1!4m1!1e3!9b0!2m2!1i10!2sCAESdkNBRVFGQnBTUTJkblNVRm9TVUZIUVVWcFFVRnZlRU5CUlZOTFVXOUxRVVF0WDNremIxbEpabDlmWDNoSlVVMURjbU5GU0RVMU9EWkhlbU5JVDFkQlFVRkJRVUp2U2w5bGRWRkJiMWxGYmxOMWFVZEJRV2xCUVE%3D!5m2!1soMA4ZryrBdHe1sQPiJ6PiA0!7e81!8m5!1b1!2b1!3b1!5b1!7b1!11m6!1e3!2e1!3spt-BR!4sbr!6m1!1i2!13m1!1e1",
     1: "https://www.google.com/maps/rpc/listugcposts?authuser=0&hl=en&gl=br&pb=!1m7!1s0x9bd5b81cec9b05%3A0xc9b595f28b3ca216!3s!6m4!4m1!1e1!4m1!1e3!2m2!1i10!2s!5m2!1snLfvZYyyCoHS1sQPxZSTiAw!7e81!8m5!1b1!2b1!3b1!5b1!7b1!11m6!1e3!2e1!3sen!4sbr!6m1!1i2!13m1!1e2",
   };
 
-  return storeUrls[storeNumber] || "";
+  return storeUrls[storeIdentifier] || "";
 }
 
 async function postReviews(reviews: Reviews) {
@@ -90,8 +90,8 @@ async function postReviews(reviews: Reviews) {
 }
 
 async function main() {
-  const storeNumber = 1; // Número da loja desejada
-  const reviews = await fetchReviews(storeNumber);
+  const storeIdentifier = 1; // Número da loja desejada
+  const reviews = await fetchReviews(storeIdentifier);
 
   console.log(reviews);
   // for (const review of reviews) {
