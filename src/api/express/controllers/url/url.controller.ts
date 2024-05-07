9e01afcf64a9c02f19f59275338d18da92b63b69
Gabriel Henrique Bueno <gabrielhenriquesi1996@gmail.com>
2024-05-07 18:21:16 -0300

    added url controller

---
+++ src/api/express/controllers/url/url.controller.ts
@@ -0,0 +1,22 @@
+// import { Request, Response } from "express";
+
+// import axios from "axios";
+
+// export class UrlController {
+//   private constructor() {}
+
+//   public static build() {
+//     return new UrlController();
+//   }
+
+//   public async create(req: Request, res: Response) {
+//     const { url } = req.body;
+
+//     const output = await getUrl(url);
+
+//     const responseData = {
+//       url,
+//     };
+//     res.status(201).json(responseData);
+//   }
+// }
