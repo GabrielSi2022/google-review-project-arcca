b5c54e2b5c960c0013743d7f30a4194a6bb24f01
Gabriel Henrique Bueno <gabrielhenriquesi1996@gmail.com>
2024-05-08 00:23:41 -0300

    fix reviews service

--- src/services/reviews/reviews.service.ts
+++ src/services/reviews/reviews.service.ts
@@ -1,39 +1,30 @@
 export type ListOutputDtoReviews = {
   reviews: {
-    classification: number;
-    date: Date;
-    text: string;
-    answer: string;
-    createdAt: Date;
-    updatedAt: Date;
-    userId: string;
-    reviewsId: string;
-    businessId: string;
+    classification: number,
+    text: string,
+    answer: string,
+    userId: string,
+    reviewsId: string,
+    businessId: string
   }[];
 };
 
 export type CreateOutputDtoReviews = {
   reviews: {
-    classification: number;
-    date: Date;
-    text: string;
-    answer: string;
-    createdAt: Date;
-    updatedAt: Date;
-    userId: string;
-    reviewsId: string;
-    businessId: string;
+    classification: number,
+    text: string,
+    answer: string,
+    userId: string,
+    reviewsId: string,
+    businessId: string
   };
 };
 
 export interface ReviewsService {
   create(
     classification: number,
-    date: Date,
     text: string,
     answer: string,
-    createdAt: Date,
-    updatedAt: Date,
     userId: string,
     reviewsId: string,
     businessId: string
